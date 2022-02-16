06f35ba1c491ee92d52ef6bae5c9a629a6d9c3b0
Rahulkrishnan R <rahulkrish.eee@gmail.com>
2022-02-16 22:11:05 +0530

    Add files via upload

---
+++ 2/hummans.js
@@ -0,0 +1,3 @@
+export default function Humman() {
+    return <h1>I am Humman</h1>
+}
---
+++ 2/message.js
@@ -0,0 +1,5 @@
+export default function Message(props) {
+    return <div>
+        <h4>Welcome to {props.place}</h4>
+    </div>
+} 
